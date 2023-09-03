f110ca2ccdf55f55d15d9c269144a351ed8a03c0
wjlee <nlpotato31@gmail.com>
2023-09-03 12:51:01 +0900

    [new] rudimentary

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gaem">
+      <div className="game-board">
+        game board
+      </div>
+      <div className="game-info">
+        game info
+      </div>
+    </div>
+  );
+}
+
+export default App;
